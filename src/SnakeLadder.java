595c40dea85612518b7daa0231e765a777e40926
Aman <aman.kumar99499@gmail.com>
2022-08-05 19:20:16 +0530

    Refactor

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -7,16 +7,17 @@ public class SnakeLadder {
             int readyPlayer1 = 0;
             int Player1Position =0;
             Random random = new Random();
-            int Dice = random.nextInt(6);
+            int Dice = random.nextInt(7);
             Player1Position = Player1Position + Dice;
             System.out.println("The Player Start from " + readyPlayer1);
             System.out.println("The Game Start from " + readyPlayer1);
             System.out.println("The Dice Rolled and Landed on " + Dice);
-            int opt = random.nextInt(3);
+            int opt = random.nextInt(4);
+            System.out.println(opt);
             switch (opt){
                 case 1:
                     Player1Position = Dice;
-                    System.out.println("Player Is In the same Position " + readyPlayer1);
+                    System.out.println("Player is in the same Position " + readyPlayer1);
                     break;
                 case 2:
                     Player1Position =(Player1Position + Dice);
@@ -24,8 +25,13 @@ public class SnakeLadder {
                     break;
                 case 3:
                     Player1Position = (Player1Position - Dice);
-                    System.out.printf("You Got SnakeBite your position is " + Player1Position);
-                    break;
+
+                    if (Player1Position < 0) {
+                        Player1Position = 0;
+                    }
+                        System.out.printf("You Got SnakeBite your position is " + Player1Position );
+                        break;
+                    }
             }
         }
-}
+
