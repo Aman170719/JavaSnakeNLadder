1f581eec0cefdfba266f9a112d643eacf4122729
Aman <aman.kumar99499@gmail.com>
2022-08-05 19:00:44 +0530

    Added

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -5,11 +5,27 @@ public class SnakeLadder {
         public static void main(String[] args) {
             System.out.println("Welcome to the Snake AND Ladder Game");
             int readyPlayer1 = 0;
-            System.out.printf("The Game Start from " + readyPlayer1);
-        }
-        void Dice(){
+            int Player1Position =0;
             Random random = new Random();
             int Dice = random.nextInt(6);
-            System.out.printf("The Dice Rolled and Landed on" + Dice);
+            Player1Position = Player1Position + Dice;
+            System.out.println("The Player Start from " + readyPlayer1);
+            System.out.println("The Game Start from " + readyPlayer1);
+            System.out.println("The Dice Rolled and Landed on " + Dice);
+            int opt = random.nextInt(3);
+            switch (opt){
+                case 1:
+                    Player1Position = Dice;
+                    System.out.println("Player Is In the same Position " + readyPlayer1);
+                    break;
+                case 2:
+                    Player1Position =(Player1Position + Dice);
+                    System.out.println("You Got ladder and your at " + Player1Position);
+                    break;
+                case 3:
+                    Player1Position = (Player1Position - Dice);
+                    System.out.printf("You Got SnakeBite your position is " + Player1Position);
+                    break;
+            }
         }
 }
