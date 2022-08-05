842b2ec536c3c48f9aae237e8d8999f51d3f53d3
Aman <aman.kumar99499@gmail.com>
2022-08-05 18:48:46 +0530

    ladderAndSnakes

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,8 +1,15 @@
-public class SnakeLadder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Snake AND Ladder Game");
+import java.sql.SQLOutput;
+import java.util.Random;
 
-        int readyPlayer1 = 0;
-        System.out.printf("The Game Start from " + readyPlayer1);
-    }
+public class SnakeLadder {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Snake AND Ladder Game");
+            int readyPlayer1 = 0;
+            System.out.printf("The Game Start from " + readyPlayer1);
+        }
+        void Dice(){
+            Random random = new Random();
+            int Dice = random.nextInt(6);
+            System.out.printf("The Dice Rolled and Landed on" + Dice);
+        }
 }
