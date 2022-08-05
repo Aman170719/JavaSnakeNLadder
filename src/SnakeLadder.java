5391a797e0d23ff0267b3f917b40f164b600424b
Aman <aman.kumar99499@gmail.com>
2022-08-05 18:00:53 +0530

    File Added

---
+++ src/SnakeLadder.java
@@ -0,0 +1,8 @@
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake AND Ladder Game");
+
+        int readyPlayer1 = 0;
+        System.out.printf("The Game Start from " + readyPlayer1);
+    }
+}
